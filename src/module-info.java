c1d1d9a7c9cf983fa95e93e4944ea2160ace3b8a
enyelb-os <enyelb.gnzlz@gmail.com>
2023-12-04 23:51:04 -0400

    update

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,7 @@ module tools.gnzlz.filetemplate {
     requires transitive tools.gnzlz.command;
     requires transitive tools.gnzlz.template;
     requires transitive tools.gnzlz.database;
-    requires tools.gnzlz.system.io;
+    requires transitive tools.gnzlz.system.io;
 
     exports tools.gnzlz.filetemplete;
     exports tools.gnzlz.filetemplete.properties;
